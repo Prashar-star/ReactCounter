40dae82fffeb8aaec780f6ba58965d2796076f41
Pankaj Prashar <60200280+Prashar-star@users.noreply.github.com>
2020-05-27 11:19:18 +1200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+    this.handleClickAdd = this.handleClickAdd.bind(this);
+    this.handleClickSub = this.handleClickSub.bind(this);
+  }
+
+  handleClickAdd() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      };
+    });
+  }
+
+  handleClickSub() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count - 1
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClickAdd}>Add</button>
+        <br />
+        <br />
+        <button onClick={this.handleClickSub}>Subtract</button>
+      </div>
+    );
+  }
+}
+export default App;
